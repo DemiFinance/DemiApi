37fc4ecd81ef0007e638bc62590981e81bc61234
Connor Adams <connor.adams@uoit.net>
2023-02-03 14:27:31 -0500

    Basic cassandra implementation

--- source/app.ts
+++ source/app.ts
@@ -6,4 +6,13 @@ const method = new Method({
     env: Environments.dev,
 });
 
+const cassandra = require('cassandra-driver');
+
+//Need to read more docs on connection
+const client = new cassandra.Client({
+    //db node ip's
+    contactPoints: ['0.0.0.0', '0.0.0.0'],
+    localDataCenter: 'datacenter1'
+});
+
 startAPI();
