cd168b8908cc7c9690202f06a8233f3e5c5b4437
Connor Adams <connor.adams@uoit.net>
2023-02-03 19:59:40 -0500

    create auth controller

---
+++ source/controllers/method/auth.ts
@@ -0,0 +1,35 @@
+import {Request, response, Response} from "express";
+import axios, { AxiosResponse } from "axios";
+
+import { Entity , Individual} from "../../models/entity"
+import { Address } from "../../models/address"
+
+import { Method, Environments } from 'method-node';
+
+
+require('dotenv').config();
+
+const method = new Method({
+    apiKey: process.env.METHOD_DEVELOPEMENTKEY!,
+    env: Environments.dev
+})
+
+
+//working
+const newAuthSession = async (request: Request, response: Response) => {
+    let id: string = request.params.id;
+
+    const token = await method.elements.createToken({
+        entity_id: id,
+        team_name: 'Demi Finance',
+        type: 'auth',
+        auth: {},
+    });
+
+    return response.status(200).json({
+        element_token: token.element_token
+    })
+}
+
+
+export default { newAuthSession };
