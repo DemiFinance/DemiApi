e1082c2603c24fe179d9a352fa923ec625514799
Connor Adams <connor.adams@uoit.net>
2023-02-03 19:58:42 -0500

    auth routing

---
+++ source/routes/auth.ts
@@ -0,0 +1,9 @@
+import express from "express"
+
+import authController from "../controllers/method/auth"
+
+const router = express.Router();
+
+router.post('/auth/:id', authController.newAuthSession);
+
+export = router;
