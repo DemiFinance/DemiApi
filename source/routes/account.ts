b704ce90e072e3159e331a9015b62cd78c8279d6
Connor Adams <connor.adams@uoit.net>
2023-02-10 00:43:15 -0500

    add account routing

---
+++ source/routes/account.ts
@@ -0,0 +1,10 @@
+import express from "express"
+import accountController from "../controllers/method/account";
+
+
+const router = express.Router();
+
+router.get('/account/:id', accountController.getAccountById);
+router.get('/accounts/:id', accountController.listAccountsByHolder);
+
+export = router
