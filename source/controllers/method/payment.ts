9b9d896c4e62d707efe0f527eba13c681f29778c
Connor Adams <connor.adams@uoit.net>
2023-02-10 12:33:17 -0500

    payment controller scaffolding

---
+++ source/controllers/method/payment.ts
@@ -0,0 +1,25 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { Request, Response} from "express";
+import { Method, Environments, IAccountListOpts } from 'method-node';
+
+const method = new Method({
+    apiKey: process.env.METHOD_DEVELOPMENTKEY!,
+    env: Environments.dev
+})
+
+const sendPayment = async (request: Request, response: Response) => {
+
+    const payment = await method.payments.create({
+        amount: 10,
+        source: request.body.sourceAccount,
+        destination: request.body.destinationAccount,
+        description: request.body.description,
+    });
+
+    return response.status(200).json({
+        "Populate me?"
+    });
+}
+
+export default { sendPayment};
