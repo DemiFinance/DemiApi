c39680f40b47af274b5101bbd7b7ce4340eadce4
Connor Adams <connor.adams@uoit.net>
2023-03-17 17:32:23 -0400

    Auto stash before revert of "Change from fetch to axios"

--- source/controllers/auth0functions.ts
+++ source/controllers/auth0functions.ts
@@ -9,27 +9,34 @@ import axios from "axios";
  * @returns The updated user object.
  */
 export async function updateUserMetadata(
-  accessToken: string,
-  userId: string,
-  metadata: { [key: string]: any }
+	accessToken: string,
+	userId: string,
+	metadata: {[key: string]: any}
 ): Promise<any> {
-  try {
-    // Define the headers to send with the API request
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-    };
+	try {
+		// Define the headers to send with the API request
+		const headers = {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${accessToken}`,
+		};
 
-    // Define the endpoint to update the user's metadata
-    const endpoint = `https://auth.demifinance.com/api/v2/users/${userId}`;
+		// Define the endpoint to update the user's metadata
+		const endpoint = `https://auth.demifinance.com/api/v2/users/${userId}`;
 
-    // Send a PATCH request to the Auth0 Management API to update the user's metadata
-    const response = await axios.patch(endpoint, { user_metadata: metadata }, { headers });
+		// Send a PATCH request to the Auth0 Management API to update the user's metadata
+		const response = await axios.patch(
+			endpoint,
+			{user_metadata: metadata},
+			{headers}
+		);
 
-    // Return the response data
-    return response.data;
-  } catch (error) {
-    console.error(`Error updating user metadata for user with ID ${userId}:`, error);
-    throw new Error("Failed to update user metadata");
-  }
+		// Return the response data
+		return response.data;
+	} catch (error) {
+		console.error(
+			`Error updating user metadata for user with ID ${userId}:`,
+			error
+		);
+		throw new Error("Failed to update user metadata");
+	}
 }
