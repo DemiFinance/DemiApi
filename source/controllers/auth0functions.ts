60d1241508fc8aae912ca25aa75daf0b7ecef466
Connor Adams <connor.adams@uoit.net>
2023-03-17 17:32:04 -0400

    Auto stash before revert of "Change from fetch to axios"

--- source/controllers/auth0functions.ts
+++ source/controllers/auth0functions.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import fetch from "node-fetch";
 
 /**
  * Update a user's metadata in Auth0.
@@ -9,27 +9,35 @@ import axios from "axios";
  * @returns The updated user object.
  */
 export async function updateUserMetadata(
-  accessToken: string,
-  userId: string,
-  metadata: { [key: string]: any }
+	accessToken: string,
+	userId: string,
+	metadata: {[key: string]: any}
 ): Promise<any> {
-  try {
-    // Define the headers to send with the API request
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-    };
+	try {
+		// Define the headers to send with the API request
+		const headers = {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${accessToken}`,
+		};
 
-    // Define the endpoint to update the user's metadata
-    const endpoint = `https://auth.demifinance.com/api/v2/users/${userId}`;
+		// Define the endpoint to update the user's metadata
+		const endpoint = `https://auth.demifinance.com/api/v2/users/${userId}`;
 
-    // Send a PATCH request to the Auth0 Management API to update the user's metadata
-    const response = await axios.patch(endpoint, { user_metadata: metadata }, { headers });
+		// Send a PATCH request to the Auth0 Management API to update the user's metadata
+		const response = await fetch(endpoint, {
+			method: "PATCH",
+			headers,
+			body: JSON.stringify({user_metadata: metadata}),
+		});
 
-    // Return the response data
-    return response.data;
-  } catch (error) {
-    console.error(`Error updating user metadata for user with ID ${userId}:`, error);
-    throw new Error("Failed to update user metadata");
-  }
+		// Parse the response JSON and return it
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error(
+			`Error updating user metadata for user with ID ${userId}:`,
+			error
+		);
+		throw new Error("Failed to update user metadata");
+	}
 }
