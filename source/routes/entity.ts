0ba63512764a1dee5774878a9fadb7b419b92d82
Connor Adams <connor.adams@uoit.net>
2023-03-20 19:40:58 -0400

    revert issuer url

--- source/routes/entity.ts
+++ source/routes/entity.ts
@@ -6,7 +6,7 @@ const {auth} = require("express-oauth2-jwt-bearer");
 
 const jwtCheck = auth({
 	audience: "https://api.demifinance.com",
-	issuerBaseURL: "https://auth.demifinance.com/",
+    issuerBaseURL: "https://dev-0u7isllacvzlfhww.us.auth0.com/",
 	tokenSigningAlg: "RS256",
 });
 
