598c7589f3d64323d7f6121515d2d6df74e26773
Connor Adams <connor.adams@uoit.net>
2023-03-20 15:00:49 -0400

    Handle export properly

--- source/routes/account.ts
+++ source/routes/account.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import accountController from "../controllers/method/account";
+import {jwtCheck} from "../globals";
 
 const router = express.Router();
 
 router.get("/account/:id", jwtCheck, accountController.getAccountById);
-router.get("/accounts/:id", jwtCheck,  accountController.listAccountsByHolder);
+router.get("/accounts/:id", jwtCheck, accountController.listAccountsByHolder);
 
 export = router;
