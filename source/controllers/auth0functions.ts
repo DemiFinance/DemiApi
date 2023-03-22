d1816e4f3eeb701362cd41ab66a706eb6b10d42e
Connor Adams <connor.adams@uoit.net>
2023-03-22 12:45:43 -0400

    change audience and client_id

--- source/controllers/auth0functions.ts
+++ source/controllers/auth0functions.ts
@@ -50,9 +50,9 @@ export async function getToken(): Promise<string> {
 		headers: {"content-type": "application/x-www-form-urlencoded"},
 		data: new URLSearchParams({
 			grant_type: "client_credentials",
-			client_id: "HNgNV6QQAj3T9ThpRMhTY0rGqAGfzeTn",
+			client_id: "zkCzuZm3qchILm3LCbYXicdPIzF90EUg",
 			client_secret: process.env.AUTH0_CLIENT_SECRET!,
-			audience: "https://dev-0u7isllacvzlfhww.us.auth0.com/api/v2/",
+			audience: "https://api.demifinance.com",
 		}),
 	};
 
