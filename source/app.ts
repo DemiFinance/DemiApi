2c2468534887947a066c4debf6aec9c2e27c2320
Connor Adams <connor.adams@uoit.net>
2023-03-28 15:30:16 -0400

    remove unused database calls

--- source/app.ts
+++ source/app.ts
@@ -3,13 +3,4 @@ dotenv.config();
 
 import {startAPI} from "./server";
 
-const cassandra = require("cassandra-driver");
-
-//Need to read more docs on connection
-const client = new cassandra.Client({
-	//db node ip's
-	contactPoints: ["0.0.0.0", "0.0.0.0"],
-	localDataCenter: "datacenter1",
-});
-
 startAPI();
