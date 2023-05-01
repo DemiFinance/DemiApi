e2e3f625f9f7845a334ca80b41679a90de78f572
Connor-Adams <connor.adams@uoit.net>
2023-04-30 20:22:07 -0400

    linting

--- source/controllers/auth0functions.ts
+++ source/controllers/auth0functions.ts
@@ -95,8 +95,8 @@ export async function pushMetadata(userId: string, metadata: UserMetadata) {
 export async function updateUserMeta(
 	accessToken: string,
 	userId: string,
-	familyName : string,
-	givenName : string,
+	familyName: string,
+	givenName: string,
 	metadata: {[key: string]: any}
 ): Promise<any> {
 	try {
@@ -105,9 +105,7 @@ export async function updateUserMeta(
 		// Send a PATCH request to the Auth0 Management API to update the user's metadata
 		const response = await axios.patch(
 			endpoint,
-			{given_name: givenName,
-			family_name: familyName,
-			app_metadata: metadata},
+			{given_name: givenName, family_name: familyName, app_metadata: metadata},
 			{
 				headers: {
 					authorization: `Bearer ${accessToken}`,
