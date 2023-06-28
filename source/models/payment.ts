550de806bc8e5669683d975d1d46a75a159b01be
Connor Adams <connor.adams@uoit.net>
2023-06-28 13:52:09 -0400

    remove some useleess stuff

--- source/models/payment.ts
+++ source/models/payment.ts
@@ -3,9 +3,6 @@
 //   import { Convert, PaymentList } from "./file";
 //
 //   const paymentList = Convert.toPaymentList(json);
-//
-// These functions will throw an error if the JSON doesn't
-// match the expected interface, even if the JSON is valid.
 
 export interface PaymentList {
 	success: boolean;
@@ -20,20 +17,20 @@ export interface Payment {
 	amount: number;
 	description: string;
 	status: string;
-	estimatedCompletionDate: Date;
-	sourceTraceID: string;
-	sourceSettlementDate: Date;
-	sourceStatus: string;
-	destinationTraceID: null | string;
-	destinationSettlementDate: Date;
-	destinationStatus: string;
-	reversalID: null;
+	estimated_completion_date: Date;
+	source_trace_id: string;
+	source_settlement_date: Date;
+	source_status: string;
+	destination_trace_id: null | string;
+	destination_settlement_date: Date;
+	destination_status: string;
+	reversal_id: null;
 	fee: Fee | null;
 	type: string;
 	error: null;
 	metadata: null;
-	createdAt: Date;
-	updatedAt: Date;
+	created_at: Date;
+	updated_at: Date;
 }
 
 export interface Fee {
@@ -42,250 +39,12 @@ export interface Fee {
 }
 
 // Converts JSON strings to/from your types
-// and asserts the results of JSON.parse at runtime
 export class Convert {
 	public static toPaymentList(json: string): PaymentList {
-		return cast(JSON.parse(json), r("PaymentList"));
+		return JSON.parse(json);
 	}
 
 	public static paymentListToJson(value: PaymentList): string {
-		return JSON.stringify(uncast(value, r("PaymentList")), null, 2);
+		return JSON.stringify(value);
 	}
 }
-
-function invalidValue(typ: any, val: any, key: any, parent: any = ""): never {
-	const prettyTyp = prettyTypeName(typ);
-	const parentText = parent ? ` on ${parent}` : "";
-	const keyText = key ? ` for key "${key}"` : "";
-	throw Error(
-		`Invalid value${keyText}${parentText}. Expected ${prettyTyp} but got ${JSON.stringify(
-			val
-		)}`
-	);
-}
-
-function prettyTypeName(typ: any): string {
-	if (Array.isArray(typ)) {
-		if (typ.length === 2 && typ[0] === undefined) {
-			return `an optional ${prettyTypeName(typ[1])}`;
-		} else {
-			return `one of [${typ
-				.map((a) => {
-					return prettyTypeName(a);
-				})
-				.join(", ")}]`;
-		}
-	} else if (typeof typ === "object" && typ.literal !== undefined) {
-		return typ.literal;
-	} else {
-		return typeof typ;
-	}
-}
-
-function jsonToJSProps(typ: any): any {
-	if (typ.jsonToJS === undefined) {
-		const map: any = {};
-		typ.props.forEach((p: any) => (map[p.json] = {key: p.js, typ: p.typ}));
-		typ.jsonToJS = map;
-	}
-	return typ.jsonToJS;
-}
-
-function jsToJSONProps(typ: any): any {
-	if (typ.jsToJSON === undefined) {
-		const map: any = {};
-		typ.props.forEach((p: any) => (map[p.js] = {key: p.json, typ: p.typ}));
-		typ.jsToJSON = map;
-	}
-	return typ.jsToJSON;
-}
-
-function transform(
-	val: any,
-	typ: any,
-	getProps: any,
-	key: any = "",
-	parent: any = ""
-): any {
-	function transformPrimitive(typ: string, val: any): any {
-		if (typeof typ === typeof val) return val;
-		return invalidValue(typ, val, key, parent);
-	}
-
-	function transformUnion(typs: any[], val: any): any {
-		// val must validate against one typ in typs
-		const l = typs.length;
-		for (let i = 0; i < l; i++) {
-			const typ = typs[i];
-			try {
-				return transform(val, typ, getProps);
-			} catch (_) {}
-		}
-		return invalidValue(typs, val, key, parent);
-	}
-
-	function transformEnum(cases: string[], val: any): any {
-		if (cases.indexOf(val) !== -1) return val;
-		return invalidValue(
-			cases.map((a) => {
-				return l(a);
-			}),
-			val,
-			key,
-			parent
-		);
-	}
-
-	function transformArray(typ: any, val: any): any {
-		// val must be an array with no invalid elements
-		if (!Array.isArray(val)) return invalidValue(l("array"), val, key, parent);
-		return val.map((el) => transform(el, typ, getProps));
-	}
-
-	function transformDate(val: any): any {
-		if (val === null) {
-			return null;
-		}
-		const d = new Date(val);
-		if (isNaN(d.valueOf())) {
-			return invalidValue(l("Date"), val, key, parent);
-		}
-		return d;
-	}
-
-	function transformObject(
-		props: {[k: string]: any},
-		additional: any,
-		val: any
-	): any {
-		if (val === null || typeof val !== "object" || Array.isArray(val)) {
-			return invalidValue(l(ref || "object"), val, key, parent);
-		}
-		const result: any = {};
-		Object.getOwnPropertyNames(props).forEach((key) => {
-			const prop = props[key];
-			const v = Object.prototype.hasOwnProperty.call(val, key)
-				? val[key]
-				: undefined;
-			result[prop.key] = transform(v, prop.typ, getProps, key, ref);
-		});
-		Object.getOwnPropertyNames(val).forEach((key) => {
-			if (!Object.prototype.hasOwnProperty.call(props, key)) {
-				result[key] = transform(val[key], additional, getProps, key, ref);
-			}
-		});
-		return result;
-	}
-
-	if (typ === "any") return val;
-	if (typ === null) {
-		if (val === null) return val;
-		return invalidValue(typ, val, key, parent);
-	}
-	if (typ === false) return invalidValue(typ, val, key, parent);
-	let ref: any = undefined;
-	while (typeof typ === "object" && typ.ref !== undefined) {
-		ref = typ.ref;
-		typ = typeMap[typ.ref];
-	}
-	if (Array.isArray(typ)) return transformEnum(typ, val);
-	if (typeof typ === "object") {
-		return typ.hasOwnProperty("unionMembers")
-			? transformUnion(typ.unionMembers, val)
-			: typ.hasOwnProperty("arrayItems")
-			? transformArray(typ.arrayItems, val)
-			: typ.hasOwnProperty("props")
-			? transformObject(getProps(typ), typ.additional, val)
-			: invalidValue(typ, val, key, parent);
-	}
-	// Numbers can be parsed by Date but shouldn't be.
-	if (typ === Date && typeof val !== "number") return transformDate(val);
-	return transformPrimitive(typ, val);
-}
-
-function cast<T>(val: any, typ: any): T {
-	return transform(val, typ, jsonToJSProps);
-}
-
-function uncast<T>(val: T, typ: any): any {
-	return transform(val, typ, jsToJSONProps);
-}
-
-function l(typ: any) {
-	return {literal: typ};
-}
-
-function a(typ: any) {
-	return {arrayItems: typ};
-}
-
-function u(...typs: any[]) {
-	return {unionMembers: typs};
-}
-
-function o(props: any[], additional: any) {
-	return {props, additional};
-}
-
-function m(additional: any) {
-	return {props: [], additional};
-}
-
-function r(name: string) {
-	return {ref: name};
-}
-
-const typeMap: any = {
-	PaymentList: o(
-		[
-			{json: "success", js: "success", typ: true},
-			{json: "data", js: "data", typ: a(r("Datum"))},
-			{json: "message", js: "message", typ: null},
-		],
-		false
-	),
-	Datum: o(
-		[
-			{json: "id", js: "id", typ: ""},
-			{json: "source", js: "source", typ: ""},
-			{json: "destination", js: "destination", typ: ""},
-			{json: "amount", js: "amount", typ: 0},
-			{json: "description", js: "description", typ: ""},
-			{json: "status", js: "status", typ: ""},
-			{
-				json: "estimated_completion_date",
-				js: "estimatedCompletionDate",
-				typ: Date,
-			},
-			{json: "source_trace_id", js: "sourceTraceID", typ: ""},
-			{json: "source_settlement_date", js: "sourceSettlementDate", typ: Date},
-			{json: "source_status", js: "sourceStatus", typ: ""},
-			{
-				json: "destination_trace_id",
-				js: "destinationTraceID",
-				typ: u(null, ""),
-			},
-			{
-				json: "destination_settlement_date",
-				js: "destinationSettlementDate",
-				typ: Date,
-			},
-			{json: "destination_status", js: "destinationStatus", typ: ""},
-			{json: "reversal_id", js: "reversalID", typ: null},
-			{json: "fee", js: "fee", typ: u(r("Fee"), null)},
-			{json: "type", js: "type", typ: ""},
-			{json: "error", js: "error", typ: null},
-			{json: "metadata", js: "metadata", typ: null},
-			{json: "created_at", js: "createdAt", typ: Date},
-			{json: "updated_at", js: "updatedAt", typ: Date},
-		],
-		false
-	),
-	Fee: o(
-		[
-			{json: "amount", js: "amount", typ: 0},
-			{json: "type", js: "type", typ: ""},
-		],
-		false
-	),
-};
