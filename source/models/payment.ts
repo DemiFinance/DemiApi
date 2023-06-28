585897efa25b47e52cb6f5df6df57b8da81e096f
Connor Adams <connor.adams@uoit.net>
2023-06-28 14:23:20 -0400

    fix linting

--- source/models/payment.ts
+++ source/models/payment.ts
@@ -5,44 +5,44 @@
 //   const paymentList = Convert.toPaymentList(json);
 
 export interface PaymentList {
-    data: Payment[];
+	data: Payment[];
 }
 
 export interface Payment {
-    id:                          string;
-    source:                      string;
-    destination:                 string;
-    amount:                      number;
-    description:                 string;
-    status:                      string;
-    estimated_completion_date:   Date;
-    source_trace_id:             string;
-    source_settlement_date:      Date;
-    source_status:               string;
-    destination_trace_id:        null | string;
-    destination_settlement_date: Date;
-    destination_status:          string;
-    reversal_id:                 null;
-    fee:                         Fee | null;
-    type:                        string;
-    error:                       null;
-    metadata:                    null;
-    created_at:                  Date;
-    updated_at:                  Date;
+	id: string;
+	source: string;
+	destination: string;
+	amount: number;
+	description: string;
+	status: string;
+	estimated_completion_date: Date;
+	source_trace_id: string;
+	source_settlement_date: Date;
+	source_status: string;
+	destination_trace_id: null | string;
+	destination_settlement_date: Date;
+	destination_status: string;
+	reversal_id: null;
+	fee: Fee | null;
+	type: string;
+	error: null;
+	metadata: null;
+	created_at: Date;
+	updated_at: Date;
 }
 
 export interface Fee {
-    amount: number;
-    type:   string;
+	amount: number;
+	type: string;
 }
 
 // Converts JSON strings to/from your types
 export class Convert {
-    public static toPaymentList(json: string): PaymentList {
-        return JSON.parse(json);
-    }
+	public static toPaymentList(json: string): PaymentList {
+		return JSON.parse(json);
+	}
 
-    public static paymentListToJson(value: PaymentList): string {
-        return JSON.stringify(value);
-    }
+	public static paymentListToJson(value: PaymentList): string {
+		return JSON.stringify(value);
+	}
 }
