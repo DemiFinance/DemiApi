ff167ca9de9e3ec3b2ceae4964c5a1c95cf3083d
Connor-Adams <connor.adams@uoit.net>
2023-06-29 10:54:16 -0400

    use params instead of body

--- source/controllers/method/payment.ts
+++ source/controllers/method/payment.ts
@@ -18,9 +18,9 @@ const getPaymentsBySourceHolder = async (
 	request: Request,
 	response: Response
 ) => {
-	try {
+	try {console.log("Attempting to get payments by sourceId: "+ request.params.id);
 		const paymentOpts: IPaymentListOpts = {
-			source_holder_id: request.body.source_holder_id,
+			source_holder_id: request.params.id,
 		};
 
 		const payment = await method.payments.list(paymentOpts);
@@ -41,8 +41,9 @@ const getPaymentsByDestination = async (
 	response: Response
 ) => {
 	try {
+		console.log("Attempting to get payments by destinationid: "+ request.params.id);
 		const paymentOpts: IPaymentListOpts = {
-			destination: request.body.destination_holder_id,
+			destination: request.params.id
 		};
 
 		const payment = await method.payments.list(paymentOpts);
