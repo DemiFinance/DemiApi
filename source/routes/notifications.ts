aa71ade36c98520d40caaa950b8ef483f219f592
Connor Adams <connor.adams@uoit.net>
2023-07-27 23:52:39 -0400

    notifications route creation

---
+++ source/routes/notifications.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import {addNotificationTokenToMetadata, getNotificationTokenByEntyityId} from "../controllers/auth0functions";
+
+const router = express.Router();
+const {auth} = require("express-oauth2-jwt-bearer");
+
+const jwtCheck = auth({
+	audience: "https://api.demifinance.com",
+	issuerBaseURL: "https://dev-0u7isllacvzlfhww.us.auth0.com/",
+	tokenSigningAlg: "RS256",
+});
+
+router.post("/addToken", jwtCheck, addNotificationTokenToMetadata);
+router.get("/:id", jwtCheck, getNotificationTokenByEntyityId);
+
+export = router;
