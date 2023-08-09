b0c19a92297fef6935309629edeee49981c90d05
Connor-Adams <connor.adams@uoit.net>
2023-08-09 14:13:02 -0400

    payment.create handler [sc-238]

---
+++ source/controllers/webhooks/payments.ts
@@ -0,0 +1,6 @@
+export async function createPayment(id: string) {
+	console.log(`Payment with id: ${id} has created`);
+	// Add your logic here
+	// If something goes wrong, throw an error
+	// throw new Error('Failed to create payment');
+}
