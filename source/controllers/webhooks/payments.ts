574538a6f7d9eecf84e7d695ab6ff03e6029f9f9
Connor Adams <connor.adams@uoit.net>
2023-08-10 14:14:16 -0400

    idk

--- source/controllers/webhooks/payments.ts
+++ source/controllers/webhooks/payments.ts
@@ -4,9 +4,10 @@ export async function createPayment(id: string) {
 	// If something goes wrong, throw an error
 	// throw new Error('Failed to create payment');
 }
+
 export async function updatePayment(id: string) {
-    console.log(`Payment with id: ${id} has updated`);
-    // Add your logic here
-    // If something goes wrong, throw an error
-    // throw new Error('Failed to update payment');
+	console.log(`Payment with id: ${id} has updated`);
+	// Add your logic here
+	// If something goes wrong, throw an error
+	// throw new Error('Failed to update payment');
 }
