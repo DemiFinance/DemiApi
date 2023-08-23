883d4f9b9718f4e7b85b456b31a81378b006df5f
Connor Adams <connor.adams@uoit.net>
2023-08-23 17:19:29 -0400

    modify query to make calling easier

--- source/database/index.ts
+++ source/database/index.ts
@@ -2,11 +2,17 @@ import {Pool} from "pg";
 
 const pool = new Pool();
 
-export const query = async (text: string, params: [any: any]) => {
+type QueryParams = {
+	text: string;
+	values?: any[];
+};
+
+export const query = async ({text, values}: QueryParams) => {
 	const start = Date.now();
-	const res = await pool.query(text, params);
+	const res = values ? await pool.query(text, values) : await pool.query(text);
 	const duration = Date.now() - start;
 	console.log("executed query", {text, duration, rows: res.rowCount});
+
 	return res;
 };
 
