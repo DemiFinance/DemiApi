6d270da8f1afb8b3ecad387e4aaa64f574224cdf
Connor Adams <connor.adams@uoit.net>
2023-09-01 10:09:00 -0400

    refactor onesignal wrapper into utility class

--- source/wrappers/onesignalWrapper.ts
+++ source/wrappers/onesignalWrapper.ts
@@ -1,34 +1,93 @@
 import OneSignal from "@onesignal/node-onesignal";
-import {ConfigurationParameters} from "@onesignal/node-onesignal/dist/configuration";
+import dotenv from "dotenv";
 
-const configParams: ConfigurationParameters = {
-	authMethods: {
-		// Fill in the properties for AuthMethodsConfiguration
-	},
-	appKey: process.env.ONESIGNAL_API_KEY || "",
-	userKey: "your-user-key",
-};
+dotenv.config();
 
-const configuration = OneSignal.createConfiguration(configParams);
+const USER_KEY = process.env.ONESIGNAL_USER_KEY || "";
+const APP_KEY = process.env.ONESIGNAL_APP_KEY || "";
 
-const client = new OneSignal.DefaultApi(configuration);
+class OneSignalUtil {
+	private static client: OneSignal.DefaultApi;
+	private static appId: string;
 
-// Function to send a notification
-export const sendNotification = (
-	message: string,
-	segments: string[] = ["All"]
-) => {
-	const notification = new OneSignal.Notification();
+	static initialize() {
+		const configuration = OneSignal.createConfiguration({
+			userKey: USER_KEY,
+			appKey: APP_KEY,
+		});
+		OneSignalUtil.client = new OneSignal.DefaultApi(configuration);
+		OneSignalUtil.appId = APP_KEY; // Assuming the APP_KEY serves as the appId.
+	}
 
-	notification.contents = {
-		en: "This is english",
-	};
+	/**
+	 * Sends a notification to a user by external ID.
+	 * @param externalId The external ID of the user.
+	 * @param message The message to send.
+	 * @param heading The heading for the notification. Default is the same as message.
+	 * @param name Optional name for the notification.
+	 */
+	static async sendNotificationByExternalId(
+		externalId: string,
+		message: string,
+		heading: string = message,
+		name?: string
+	): Promise<void> {
+		const notification = new OneSignal.Notification();
+		notification.app_id = OneSignalUtil.appId;
+		notification.contents = {en: message};
+		notification.headings = {en: heading};
+		notification.include_external_user_ids = [externalId];
+		if (name) {
+			notification.name = name;
+		}
 
-	notification.delivery_time_of_day = "9:00AM";
+		try {
+			const response = await OneSignalUtil.client.createNotification(
+				notification
+			);
+			console.log("Notification sent", response);
+		} catch (e) {
+			console.error("Error sending notification", e);
+		}
+	}
 
-	notification.external_id = ""; //get externalid based on who owns the notification trigger
+	/**
+	 * Sends a notification to a user by external ID, scheduled for a later date and time.
+	 * @param externalId The external ID of the user.
+	 * @param message The message to send.
+	 * @param deliverAt The date and time to deliver the notification (ISO string format).
+	 * @param heading The heading for the notification. Default is the same as message.
+	 * @param name Optional name for the notification.
+	 */
+	static async sendScheduledNotification(
+		externalId: string,
+		message: string,
+		deliverAt: string,
+		heading: string = message,
+		name?: string
+	): Promise<void> {
+		const notification = new OneSignal.Notification();
+		notification.app_id = OneSignalUtil.appId;
+		notification.contents = {en: message};
+		notification.headings = {en: heading};
+		notification.include_external_user_ids = [externalId];
+		notification.send_after = new Date(deliverAt).toISOString();
+		if (name) {
+			notification.name = name;
+		}
 
-	return client.createNotification(notification);
-};
+		try {
+			const response = await OneSignalUtil.client.createNotification(
+				notification
+			);
+			console.log("Scheduled notification sent", response);
+		} catch (e) {
+			console.error("Error sending scheduled notification", e);
+		}
+	}
+}
 
-// You can export more functions here if needed
+// Initialize the client upon importing the module.
+OneSignalUtil.initialize();
+
+export default OneSignalUtil;
