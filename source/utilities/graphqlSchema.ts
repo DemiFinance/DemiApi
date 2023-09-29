a0899da4811ee38bda1a9c34656ab13f05ab0eb0
Connor Adams <connor.adams@uoit.net>
2023-09-29 17:47:16 -0400

    is apollo my problem?

--- source/utilities/graphqlSchema.ts
+++ source/utilities/graphqlSchema.ts
@@ -2,22 +2,33 @@ import {gql} from "graphql-tag";
 
 // Define your GraphQL schema here. This is just a placeholder and should be replaced with the actual schema for the Quiltt.io API.
 const schema = gql`
-	type User {
-		id: ID!
-		name: String!
-		email: String!
-	}
-
-	type Post {
-		id: ID!
-		title: String!
-		content: String!
-		author: User!
-	}
-
-	type Query {
-		users: [User]
-		posts: [Post]
+	query getAccount($id: ID!) {
+		account(id: $id) {
+			id
+			name
+			type
+			liability {
+				id
+				type
+				creditCard {
+					id
+					issuer
+					number
+					creditLimit
+					availableCredit
+					paymentDueDate
+					paymentDueAmount
+					minimumPaymentDue
+					transactions {
+						id
+						amount
+						date
+						description
+						category
+					}
+				}
+			}
+		}
 	}
 `;
 
