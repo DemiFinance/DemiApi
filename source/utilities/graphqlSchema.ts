531268cd601ae10337444d215961febd44645a47
Connor Adams <connor.adams@uoit.net>
2023-10-16 12:38:20 -0400

    add gql query for account and transaction data

--- source/utilities/graphqlSchema.ts
+++ source/utilities/graphqlSchema.ts
@@ -1,7 +1,7 @@
 import {gql} from "graphql-tag";
 
 // Define your GraphQL schema here. This is just a placeholder and should be replaced with the actual schema for the Quiltt.io API.
-const schema = gql`
+export const schema = gql`
 	query getAccount($id: ID!) {
 		account(id: $id) {
 			id
@@ -32,4 +32,95 @@ const schema = gql`
 	}
 `;
 
-export default schema;
+export const AccountsSchema = gql`
+	query SpendingAccountsWithTransactionsQuery {
+		account(id: "acct_12uag3KIyeQsoqGhAmabGg") {
+			sources {
+				... on MxAccount {
+					accountNumber
+					apr
+					apy
+					availableBalance
+					availableCredit
+					balance
+					cashBalance
+					cashSurrenderValue
+					createdAt
+					creditLimit
+					currencyCode
+					dayPaymentIsDue
+					deathBenefit
+					guid
+					holdingsValue
+					insuredName
+					institutionCode
+					interestRate
+					isClosed
+					lastPayment
+					lastPaymentAt
+					loanAmount
+					maturesOn
+					memberGuid
+					minimumBalance
+					minimumPayment
+					name
+					originalBalance
+					paymentDueAt
+					payoffBalance
+					payOutAmount
+					premiumAmount
+					startedOn
+					subtype
+					totalAccountValue
+					type
+					updatedAt
+					userGuid
+				}
+			}
+		}
+	}
+`;
+
+export const TransactionsSchema = gql`
+	query TransactionsByAccountId {
+		account(id: "acct_12uag3KIyeQsoqGhAmabGg") {
+			transactions {
+				source(type: MX) {
+					... on MxTransaction {
+						accountGuid
+						amount
+						category
+						checkNumberString
+						createdAt
+						currencyCode
+						date
+						description
+						guid
+						isBillPay
+						isDirectDeposit
+						isExpense
+						isFee
+						isIncome
+						isInternational
+						isOverdraftFee
+						isPayrollAdvance
+						latitude
+						longitude
+						memberGuid
+						memo
+						merchantCategoryCode
+						merchantGuid
+						originalDescription
+						postedAt
+						status
+						topLevelCategory
+						transactedAt
+						type
+						updatedAt
+						userGuid
+					}
+				}
+			}
+		}
+	}
+`;
