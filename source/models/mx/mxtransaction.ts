9a0ee860846589a27f5596c677a4d15d55d4c05d
Connor Adams <connor.adams@uoit.net>
2024-01-16 22:07:37 -0500

    this is a terrible name but ill deal with it later i guess
    build model for plaid transactions

--- source/models/mx/mxtransaction.ts
+++ source/models/mx/mxtransaction.ts
@@ -32,9 +32,47 @@ export type MxTransaction = {
 	userGuid: string;
 };
 
+export type PlaidTransaction = {
+	accountId: string;
+	amount: number;
+	isoCurrencyCode: string | null;
+	unofficialCurrencyCode: string | null;
+	category: string[];
+	checkNumber: string | null;
+	categoryId: string;
+	date: string;
+	location: {
+		address: string | null;
+		city: string | null;
+		lat: number | null;
+		lon: number | null;
+		state: string | null;
+		storeNumber: string | null;
+		zip: string | null;
+	};
+	name: string;
+	merchantName: string | null;
+	originalDescription: string | null;
+	paymentMeta: {
+		referenceNumber: string | null;
+		ppdId: string | null;
+		payee: string | null;
+		byOrderOf: string | null;
+		payer: string | null;
+		payment_method: string | null;
+		payment_processor: string | null;
+		reason: string | null;
+	};
+	pending: boolean;
+	pendingTransactionId: string | null;
+	accountOwner: string | null;
+	transactionId: string;
+	transactionType: string;
+};
+
 export type TransactionObject = {
 	__typename: string;
-	source: MxTransaction & {__typename: string}; // Include __typename in the source object type
+	source: (MxTransaction | PlaidTransaction) & {__typename: string}; // Include __typename in the source object type
 };
 
 export type AccountObject = {
