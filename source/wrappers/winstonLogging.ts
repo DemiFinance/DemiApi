95cef3b93c091ecd2ae96fb3e0dbe16b145ce05b
Connor-Adams <connor.adams@uoit.net>
2024-05-08 21:28:45 -0400

    change logging stuff

--- source/wrappers/winstonLogging.ts
+++ source/wrappers/winstonLogging.ts
@@ -1,21 +1,30 @@
 import * as dotenv from "dotenv";
+import { transports } from "winston";
 dotenv.config();
 const {createLogger, format} = require("winston");
-const DatadogWinston = require("datadog-winston");
+// const DatadogWinston = require("datadog-winston");
 
 const logger = createLogger({
 	level: "info",
 	exitOnError: false,
 	format: format.json(),
+	transports: [
+		new transports.Console({
+			format: format.combine(format.colorize(), format.simple()),
+		}),
+		new transports.File({
+			filename: "demiLogs.log",
+		}),
+	],
 });
 
-logger.add(
-	new DatadogWinston({
-		apiKey: process.env.DD_API_KEY,
-		hostname: process.env.DATADOG_HOSTNAME,
-		service: process.env.DD_SERVICE,
-		ddsource: "nodejs",
-	})
-);
+// logger.add(
+// 	new DatadogWinston({
+// 		apiKey: process.env.DD_API_KEY,
+// 		hostname: process.env.DATADOG_HOSTNAME,
+// 		service: process.env.DD_SERVICE,
+// 		ddsource: "nodejs",
+// 	})
+// );
 
 export default logger;
